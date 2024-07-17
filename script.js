50dbc541f2375ba1d12c6dae913fa70966d28952
abdulmueez144 <abdulmueezsiddiqui144@gmail.com>
2024-07-17 16:33:09 +0500

    Scripting Done!, password generator ready now :)

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
-let inputSlider = document.getElementById('inputSlider');
-let sliderValue = document.getElementById('sliderValue');
+let inputSlider = document.getElementById("inputSlider");
+let sliderValue = document.getElementById("sliderValue");
+let passBox = document.getElementById("passBox");
+let lowercase = document.getElementById("lowercase");
+let uppercase = document.getElementById("uppercase");
+let numbers = document.getElementById("numbers");
+let symbols = document.getElementById("symbols");
+let genBtn = document.getElementById("genBtn");
+let copyIcon = document.getElementById("copyIcon");
 
+// Showing input slider value
 sliderValue.textContent = inputSlider.value;
-inputSlider.addEventListener('input', ()=>{
-    sliderValue.textContent = inputSlider.value;
-})
+inputSlider.addEventListener("input", () => {
+  sliderValue.textContent = inputSlider.value;
+});
+
+genBtn.addEventListener("click", () => {
+  passBox.value = generatePassword();
+});
+
+// Function to generate password
+let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowerChars = "abcdefghijklomnpqrstuvwxyz";
+let allNumbers = "0123456789";
+let allSymbols = "!@#$%^&*()_+=-/|';:><,.[]{}`~";
+function generatePassword() {
+  let genPassword = "";
+  let allChars = "";
+  allChars += lowercase.checked ? lowerChars : "";
+  allChars += uppercase.checked ? upperChars : "";
+  allChars += numbers.checked ? allNumbers : "";
+  allChars += symbols.checked ? allSymbols : "";
+
+  let i = 1;
+  while (i <= inputSlider.value) {
+    genPassword += allChars.charAt(Math.floor(Math.random() * allChars.length));
+    i++;
+  }
+  return genPassword;
+}
+
+copyIcon.addEventListener("click", () => {
+  if (passBox.value != "" || passBox.value.length >= 1) {
+    navigator.clipboard.writeText(passBox.value);
+    copyIcon.innerText = "check";
+    copyIcon.title = "Password Copied!";
+    setTimeout(() => {
+      copyIcon.innerHTML = "content_copy";
+      copyIcon.title = "";
+    }, 2000);
+  }
+});
